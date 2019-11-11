cd1ef73815b66c56986b7bb2e73648e16c903209
Tammie Chung <tammie.chung11@gmail.com>
2019-11-11 17:50:33 -0500

    complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,18 @@
 class Store < ActiveRecord::Base
+
   has_many :employees
+
   validates :name, length: { minimum: 3 }
   validates_numericality_of :annual_revenue, on: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_destroy :check_empty
+
+  private
+    def check_empty
+      if self.employees.size >= 1
+        errors[:base] << "Can't be destroyed because there are employees!"
+        return false
+      end
+    end
+
 end
